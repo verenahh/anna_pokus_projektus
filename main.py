3f9585e9d685f0b2a8f07a7ed8699a4b7b2af78d
verenahh <hny.verena@gmail.com>
2023-04-11 12:36:37 +0200

    Muchneededayuda

---
+++ main.py
@@ -0,0 +1,28 @@
+# Printe die Spielerklärung
+print("Willkommen beim ZeitvertreiberFinder!")
+print("Das sind die Spielregeln:")
+print("*insert Spielregeln")
+
+#schreibe eine logik, bei der du den spieler fragst, ob er das Programm wie auch immer des hast starten mag. Wenn die Eingabe ja is, startet des Programm, wenn nein soll er ausloggen "Schade, vielleicht hast du später ja Lust den xxx auszuprobieren."
+
+
+#10 Fragen
+print("Die erste Frage")
+antwort1 = input()
+
+#so und des mochst jetzt 10 moi
+
+#wennst brav deine 10 Fragen gmacht hast, keman ma jetzt zum "auswerten"
+
+def vorschlag1(antwort1, antwort2):
+    if antwort1 == "XY":
+        if antwort2 == "kdlfj"
+            print("Gehe spazieren")
+        elif antwort2 == "kdlsdf"
+    else: 
+        print("whatever")
+
+#nach deine 10-15 Freizeitaktivitäten funktionen machst du den eigentlichen Ablauf
+#dazu schiebst de  funktionen ÜBER de Fragen und machst an while loop um die Fragestellungen herum
+#ev. noch feedback in seperatem file speichern
+    
